86522ecc8c5774c6ad8f9563c466ca73ebc2a289
关楚添 <guanchutian@guanchutiandeMac-mini.local>
2026-04-08 00:24:26 -0400

    chore(02-03): add jest config to worktree for test execution

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: 'tsconfig.json',
+      jsx: 'react-jsx',
+    }],
+  },
+};
+
+export default config;
